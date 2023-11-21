9bead4e72e74c3bb2bbfaa5f6e07e5d1eaca550a
jarnsorm <150379806+jarnsorm@users.noreply.github.com>
2023-11-21 19:28:41 +0300

    Add files via upload

--- random_number(refactored).py
+++ random_number(refactored).py
@@ -2,12 +2,14 @@ import random
 def game():
     my_num = random.randint(1, 10)
     print('Я загадал число. Угдай его')
-    while number != my_num:
+    while my_num:
         number = int(input('Введи число: '))
         if number > my_num:
             print('Твое число больше загаданного')
         elif number < my_num:
             print('Твое число меньше загаданного')
-    print('Молодец! Ты угадал число!')
+        else:
+            print('Молодец! Ты угадал число!')
+            break
 
 game()
